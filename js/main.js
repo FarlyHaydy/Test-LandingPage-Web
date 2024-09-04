9c71269aec38108d168ec9b6dc7b170b38a8bbd7
FarlyHaydy <farlyhay1@gmail.com>
2024-09-04 21:25:30 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -4,15 +4,3 @@ function scrollToTop() {
         behavior:'smooth'
     });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('download-cv-btn').addEventListener('click', function() {
-        const link = document.createElement('a');
-        link.href = '/cv/Farlyhaydy H.Djalil-CV.pdf';
-        link.download = 'Farly_CV.pdf'; 
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-        
-    });
-});
